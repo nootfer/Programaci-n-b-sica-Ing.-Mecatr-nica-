0c8cc8c0780d70ea83bb6ba558ad98b8ec10b367
nootfer <f9715139@gmail.com>
2025-05-13 14:13:36 -0600

    Add files via upload

---
+++ Apuntador 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int x=17, y;
+    int *p;
+    p= &x;
+    
+    printf("El valor de x es %d\n", *p);
+    y=*p+3;
+    printf("El valor de y es %d\n", y);
+
+    
+    system("Pause");
+    return 0;
+}
